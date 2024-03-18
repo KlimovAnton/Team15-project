a4099928cbc48d6b05cf35807b04b1f8557fb782
IrinaPapulova <142151842+IrinaPapulova@users.noreply.github.com>
2024-03-19 00:47:10 +0200

    corected function isInViewport

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,24 +1,31 @@
-const coversSection = document.getElementById('covers');
+const coversSection = document.querySelector('.section-cover');
+let setTimeoutId;
+let coverPictureArray = Array.from(
+  coversSection.querySelectorAll('.cover-picture')
+);
 
-window.addEventListener('scroll', () => {
-  if (isInViewport(coversSection)) {
-    coversSection.querySelectorAll('.cover-line').forEach((item, index) => {
-      setTimeout(() => {
-        item.classList.add('show');
-      }, index * 100);
-    });
-  }
-});
+window.addEventListener('scroll', isInViewport);
 
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
+function isInViewport() {
+  const rect = coversSection.getBoundingClientRect();
+
+  if (
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  ) {
+    coverPictureArray.forEach((item, index) => {
+      setTimeoutId = setTimeout(() => {
+        item.classList.add('show');
+      }, index * 50);
+    });
+  } else {
+    clearTimeout(setTimeoutId);
+    coverPictureArray.forEach(item => {
+      item.classList.remove('show');
+    });
+  }
 }
-
 export { isInViewport };
