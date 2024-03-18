3a58a322a1daa91f523ef3150a6fe43a883f50c0
Тетяна Жульєва <128372881+TanyaJulieva@users.noreply.github.com>
2024-03-18 14:55:11 +0100

    footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -5,20 +5,59 @@ const elements = {
   input: document.querySelector('.js-input'),
 };
 
-console.log(elements.input);
+elements.input.setAttribute('aria-invalid', false);
+elements.input.setAttribute('aria-valid', false);
+const errorBox = document.createElement('div');
+errorBox.classList.add('error');
+const succesBox = document.createElement('div');
+succesBox.classList.add('succes');
 
-elements.input.addEventListener('change', handlerChange);
+function initValidation(form) {
+  form.setAttribute('novalidate', '');
 
-function handlerChange(evt) {
-  console.log(evt);
-  const isValid = evt.target.checkValidity();
-  console.log(isValid);
+  form.addEventListener('submit', event => {
+    const allValid = form.checkValidity();
+    if (!allValid) {
+      event.preventDefault();
+    }
+  });
+ 
+  elements.input.insertAdjacentElement('afterend', errorBox);
+  elements.input.insertAdjacentElement('afterend', succesBox);
+
+  elements.input.addEventListener('blur', () => {
+    elements.input.checkValidity();
+  });
+
+  elements.input.addEventListener('input', () => {
+    const valid = elements.input.checkValidity();
+    if (valid) {
+      elements.input.setAttribute('aria-invalid', false);
+      elements.input.setAttribute('aria-valid', true);
+      succesBox.textContent = 'Succes!';
+      errorBox.textContent = '';
+      return;
+    } 
+    else {
+      elements.input.setAttribute('aria-valid', false);
+      succesBox.textContent = ''
+      elements.input.setAttribute('aria-invalid', true);
+      errorBox.textContent = 'Invalid email, try again!';
+    }
+  });
 }
+
+initValidation(elements.form);
 
 elements.form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
   evt.preventDefault();
+  
+  succesBox.classList.remove('succes');
+  elements.input.setAttribute('aria-valid', false);
+  succesBox.textContent = '';
+
 
   const { email, comment } = evt.currentTarget.elements;
 
