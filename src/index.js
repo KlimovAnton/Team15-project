5b595b8cc3c9148e1de1987b2575d89880ff7d1c
Anton <antonklim6@gmail.com>
2024-03-20 00:03:34 +0100

    add # to header and script open close

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { isInViewport } from "./js/cover.js";
-import { toggleMenu } from "./js/header-modal.js";
+import { openModal } from "./js/header-modal.js";
+import { closeModal } from "./js/header-modal.js";
 import { elements, resetStyles, setSuccessStyles, initValidation, getMessage, handlerSubmit, handlerClick } from "./js/footer.js"
