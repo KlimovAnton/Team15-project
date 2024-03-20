ad758a443f5f007c157026b1a7c5c46ae5728d34
IrinaPapulova <142151842+IrinaPapulova@users.noreply.github.com>
2024-03-20 04:41:01 +0200

    html-css-correction

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,31 +1,27 @@
+
 const coversSection = document.querySelector('.section-cover');
-let setTimeoutId;
-let coverPictureArray = Array.from(
-  coversSection.querySelectorAll('.cover-picture')
-);
+let coverPictureArray = Array.from(coversSection.querySelectorAll('.cover-picture'));
 
 window.addEventListener('scroll', isInViewport);
-
 function isInViewport() {
   const rect = coversSection.getBoundingClientRect();
 
   if (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   ) {
-    coverPictureArray.forEach((item, index) => {
-      setTimeoutId = setTimeout(() => {
-        item.classList.add('show');
-      }, index * 50);
+    coverPictureArray.forEach((item) => {
+      item.classList.add('show');
     });
   } else {
-    clearTimeout(setTimeoutId);
     coverPictureArray.forEach(item => {
       item.classList.remove('show');
     });
   }
 }
+
+
+
 export { isInViewport };
