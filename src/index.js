2ac4e9815cec4d356bb8da1c4d1baff40862a0f9
LesyaKot <lesyakot83@gmail.com>
2024-03-20 14:16:40 +0500

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { isInViewport } from "./js/cover.js";
+
+
+
+
+
+
+
+
+
+
+
 import { openModal } from "./js/header-modal.js";
 import { closeModal } from "./js/header-modal.js";
 import { elements, resetStyles, setSuccessStyles, initValidation, getMessage, handlerSubmit, handlerClick } from "./js/footer.js"
