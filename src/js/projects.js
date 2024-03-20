5de85bd210e15574ad9d1e561b9afc94375df697
Anton <antonklim6@gmail.com>
2024-03-20 01:57:54 +0100

    add projects swiper and refactoring reviews code  (broken img-tabl-2 and idk, btn reviews (it is bug))

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,76 +1,29 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-async function getReviews() {
-  try {
-    const response = await fetch(
-      'https://portfolio-js.b.goit.study/api/reviews'
-    );
-    const reviews = await response.json();
+const projectSliderElement = document.querySelector('.project-container');
+const projectSliderProps = {
+  speed: 900,
+  allowTouchMove: false,
+  navigation: {
+    nextEl: '.slide-next',
+    prevEl: '.slide-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+};
+const projectSlider = new Swiper(projectSliderElement, projectSliderProps);
 
-    if (reviews.length === 0) {
-      alert('Not found');
-      return;
-    }
 
-    renderReviews(reviews);
 
-    const swiperBtn = document.querySelector('.swiper-btn-wrap');
-    swiperBtn.classList.remove('hidden');
-  } catch (error) {
-    console.error('Error fetching reviews:', error);
-    alert('An error occurred while fetching reviews');
-  }
-}
-
-function renderReviews(reviews) {
-  const swiperWrapper = document.querySelector('.reviews-wrapper');
-  swiperWrapper.innerHTML = '';
-  let reviewsList = '';
-  for (const { author, avatar_url, review } of reviews) {
-    reviewsList += `
-      <li class="swiper-slide swiper-style">
-        <img class="review-avatar" src="${avatar_url}" alt="${author}" width="48" height="48" />
-        <h3 class="review-autor">${author}</h3>
-        <p class="review-text">${review}</p>
-      </li>
-    `;
-  }
-  swiperWrapper.insertAdjacentHTML('beforeend', reviewsList);
-}
-
-getReviews();
+// getReviews();
 
 // const swiperWrapper = document.querySelector('.swiper-wrapper');
-const swiperContainer = document.querySelector('.reviews-wrapper-check');
-const swiperWrapper = {
-  speed: 900,
-  navigation: {
-        nextEl: '.swiper-btn-next',
-        prevEl: '.swiper-btn-prev',
-      },
-      keyboard: {
-        enabled: true,
-      },
-      mousewheel: true,
-      touch: true,
-      slidesPerView: 1,
-      spaceBetween: 16,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 16,
-        },
-        1440: {
-          slidesPerView: 4,
-          spaceBetween: 16,
-        },
-      },
-      allowSlideNext: true,
-    };
+// const swiperContainer = document.querySelector('.swiper-container');
 
-const reviewSlider = new Swiper(swiperContainer, swiperWrapper);
-// const swiper = new Swiper('.reviews-wrapper', {
+// const swiper = new Swiper('.swiper-container', {
 //   navigation: {
 //     nextEl: '.swiper-btn-next',
 //     prevEl: '.swiper-btn-prev',
