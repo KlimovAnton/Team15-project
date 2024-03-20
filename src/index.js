3173619cc7d9c27d168aa62ab9b775fee642189d
Тетяна Жульєва <128372881+TanyaJulieva@users.noreply.github.com>
2024-03-20 20:56:05 +0100

    js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./js/header-modal.js";
-import './js/projects.js';
-import './js/faq.js';
-import './js/render-api.js';
-import { isInViewport } from "./js/cover.js";
-import './js/reviews.js';
+// import "./js/header-modal.js";
+// import './js/projects.js';
+// import './js/faq.js';
+// import './js/render-api.js';
+// import { isInViewport } from "./js/cover.js";
+// import './js/reviews.js';
 import { elements, resetStyles, setSuccessStyles, initValidation, getMessage, handlerSubmit, handlerClick } from "./js/footer.js";
 
 
