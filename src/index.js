1cd7e6dc902b06f175a765305f96813b51fb44e5
Anton <antonklim6@gmail.com>
2024-03-22 16:12:32 +0100

    swaping import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import "./js/header-modal.js";
 import "./js/about.js";
-import "./js/projects.js";
 import './js/faq.js';
 import './js/render-api.js';
 import { isInViewport } from "./js/cover.js";
 import './js/reviews.js';
 import "./js/footer.js";
-
+import "./js/projects.js";
 
